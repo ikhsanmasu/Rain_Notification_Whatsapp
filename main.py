69c38ec74c25c553e1b99aa025390d1f246751c8
Ikhsan Maulana <76894210+ikhsanmasu@users.noreply.github.com>
2023-10-23 12:50:15 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from twilio.rest import Client
+import os
+
+lat = -0.947083
+lon = 100.417183
+API_key = "16c80aa7623d583b2827000e60b49915"
+OWM_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
+
+weather_param = {
+    "lat": lat,
+    "lon": lon,
+    "appid": API_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(OWM_Endpoint, params=weather_param)
+response.raise_for_status()
+
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["hourly"][:12]:
+    condition_code = hour_data["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+        break
+
+account_sid = os.environ.get("sid")
+auth_token = os.environ.get("token")
+client = Client(account_sid, auth_token)
+
+if will_rain:
+    message = client.messages.create(
+                                  body='Hari ini akan hujan, bawalah payung!',
+                                  from_='whatsapp:+xxxxxxxxxxx',
+                                  to='whatsapp:xxxxxxxxxxxx'
+                              )
+    print(message.status)
+
